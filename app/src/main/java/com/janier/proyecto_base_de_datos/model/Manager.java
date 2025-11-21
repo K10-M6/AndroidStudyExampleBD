cf980d113778b10b6332946bcb4522f450c641bc
Janier Cerón <janierjceron2044@gmail.com>
2025-11-21 12:20:16 -0500

    Gear second :D

--- app/src/main/java/com/janier/proyecto_base_de_datos/model/Manager.java
+++ app/src/main/java/com/janier/proyecto_base_de_datos/model/Manager.java
@@ -10,6 +10,16 @@ public class Manager {
     private ConexionBD conexionBD;
     private SQLiteDatabase db;
     public Manager(Context context){
+        //llamo la conexión
         conexionBD = new ConexionBD(context);
+    }
+    public void openBD(){
+        //abre la base de datos modo escritura
+        db= conexionBD.getWritableDatabase();
+
+    }
+    public void openBdRd(){
+        //abre la base de datos modo lectura
+        db= conexionBD.getReadableDatabase();
     }
 }
